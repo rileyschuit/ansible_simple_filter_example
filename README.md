b88c13eb2a8482fece827dddf77d4975de8c8c5c
Riley Schuit <rischuit@teksystems.com>
2022-01-28 17:00:57 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EXTREMLY simple filter example
+
+Summary:  I get this question a lot, so I figure I'll make it a repo
---
+++ roles/simple_filter_example/filter_plugins/name_doesnt_matter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import json
+import os
+
+def filter_unique(things):
+  seen = set()
+  unique_things = []
+
+  for thing in things:
+    if thing not in seen:
+      seen.add(thing)
+      unique_things.append(thing)
+
+  return unique_things
+
+
+class FilterModule(object):
+    filter_map = {
+        'unique_things': filter_unique,
+    }
+
+    def filters(self):
+        return self.filter_map
